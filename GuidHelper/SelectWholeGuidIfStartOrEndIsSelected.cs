93bad43088eadecc266d95c27399c1afc9737a23
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2016-08-02 19:24:35 +0200

    extract methods

--- GuidHelper/SelectWholeGuidIfStartOrEndIsSelected.cs
+++ GuidHelper/SelectWholeGuidIfStartOrEndIsSelected.cs
@@ -41,48 +41,57 @@ namespace Kbg.NppPluginNET.GuidHelper
 			switch (selectionLength)
 			{
 				case GuidHelperConstants.StartPartOfGuidLength:
-				{
-					var isMatch = (lineContent.Length >= GuidHelperConstants.Regexlength + relativeLineOfset && GuidHelperConstants.GuidRex.IsMatch(lineContent, relativeLineOfset));
-					if (!isMatch)
-						return;
-
-					var end = new Position(absolutePosOfLine.Value + relativeLineOfset + GuidHelperConstants.Regexlength);
-					if (isLeftToRigthSelection)
-					{
-						scintillaGateway.SetAnchor(absolutePosStart);
-						scintillaGateway.SetCurrentPos(end);
-					}
-					else // "reverse" selection from right-to-left
-					{
-						scintillaGateway.SetAnchor(end);
-						scintillaGateway.SetCurrentPos(absolutePosStart);
-					}
-				}
+					FirstPartOfGuidPossiblySelected(relativeLineOfset, lineContent, absolutePosOfLine, isLeftToRigthSelection, scintillaGateway, absolutePosStart);
 					break;
 
 				case GuidHelperConstants.EndPartOfGuidLength:
-				{
-					var isMatch = relativeLineOfset >= LengthGuidExceptLastPart
-						&& lineContent.Length >= GuidHelperConstants.Regexlength  
-						&& GuidHelperConstants.GuidRex.IsMatch(lineContent, relativeLineOfset - LengthGuidExceptLastPart);
-					if (!isMatch)
-						return;
-
-					var finalStartSelection = new Position(absolutePosStart.Value - LengthGuidExceptLastPart);
-					var end = new Position(finalStartSelection.Value + GuidHelperConstants.Regexlength);
-
-					if (isLeftToRigthSelection)
-					{
-						scintillaGateway.SetAnchor(finalStartSelection);
-						scintillaGateway.SetCurrentPos(end);
-					}
-					else // "reverse" selection from right-to-left
-					{
-						scintillaGateway.SetAnchor(end);
-						scintillaGateway.SetCurrentPos(finalStartSelection);
-					}
-				}
+					LastPartOfGuidPossiblySelected(relativeLineOfset, lineContent, absolutePosStart, isLeftToRigthSelection, scintillaGateway);
 					break;
+			}
+		}
+
+		private void LastPartOfGuidPossiblySelected(int relativeLineOfset, string lineContent, Position absolutePosStart,
+			bool isLeftToRigthSelection, IScintillaGateway scintillaGateway)
+		{
+			var isMatch = relativeLineOfset >= LengthGuidExceptLastPart
+			              && lineContent.Length >= GuidHelperConstants.Regexlength
+			              && GuidHelperConstants.GuidRex.IsMatch(lineContent, relativeLineOfset - LengthGuidExceptLastPart);
+			if (!isMatch)
+				return;
+
+			var finalStartSelection = new Position(absolutePosStart.Value - LengthGuidExceptLastPart);
+			var end = new Position(finalStartSelection.Value + GuidHelperConstants.Regexlength);
+
+			if (isLeftToRigthSelection)
+			{
+				scintillaGateway.SetAnchor(finalStartSelection);
+				scintillaGateway.SetCurrentPos(end);
+			}
+			else // "reverse" selection from right-to-left
+			{
+				scintillaGateway.SetAnchor(end);
+				scintillaGateway.SetCurrentPos(finalStartSelection);
+			}
+		}
+
+		private void FirstPartOfGuidPossiblySelected(int relativeLineOfset, string lineContent, Position absolutePosOfLine, bool isLeftToRigthSelection, 
+			IScintillaGateway scintillaGateway, Position absolutePosStart)
+		{
+			var isMatch = lineContent.Length >= GuidHelperConstants.Regexlength + relativeLineOfset 
+							&& GuidHelperConstants.GuidRex.IsMatch(lineContent, relativeLineOfset);
+			if (!isMatch)
+				return;
+
+			var end = new Position(absolutePosOfLine.Value + relativeLineOfset + GuidHelperConstants.Regexlength);
+			if (isLeftToRigthSelection)
+			{
+				scintillaGateway.SetAnchor(absolutePosStart);
+				scintillaGateway.SetCurrentPos(end);
+			}
+			else // "reverse" selection from right-to-left
+			{
+				scintillaGateway.SetAnchor(end);
+				scintillaGateway.SetCurrentPos(absolutePosStart);
 			}
 		}
 	}
